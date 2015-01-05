41777001cff40ba0a7314d7ae06443f20b765e0b
Bryan Weber <bryan@cobenian.com>
2015-01-04 23:30:03 -0500

    adding the start of an actual cli

--- lib/expcap/cli.ex
+++ lib/expcap/cli.ex
@@ -1,1 +1,36 @@
-ExPcap.from_file("/tmp/dns.cap") |> String.Chars.to_string |> IO.puts
+defmodule ExPcap.CLI do
+  @moduledoc """
+
+  """
+  def main(argv) do
+    run(argv)
+  end
+
+  def run(argv) do
+    argv |> parse_args |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv,
+      switches: [help: :boolean, file: :string],
+      aliases: [h: :help, f: :file]
+    )
+    case parse do
+      { [ help: true ], _, _ } -> :help
+      { [ file: name ], _, _ } -> [file: name]
+      _ -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+      --file, -f <file>       The PCAP file to use
+      --help, -h              Print this message
+    """
+    System.halt(0)
+  end
+
+  def process([file: file]) do
+    file |> ExPcap.from_file |> String.Chars.to_string |> IO.puts
+  end
+end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,12 @@ defmodule ExPcap.Mixfile do
     [app: :expcap,
      version: "0.0.1",
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     escript: escript]
+  end
+
+  def escript do
+    [main_module: ExPcap.CLI]
   end
 
   # Configuration for the OTP application
